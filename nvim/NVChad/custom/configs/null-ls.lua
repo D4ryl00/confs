2088e1e84b8195f949d07e8d1a338172f752480b
Remi BARBERO <d4ryl00@Remis-MacBook-Pro-M1.local>
2023-09-18 11:50:16 +0200

    add nvchad config
    Signed-off-by: Remi BARBERO <d4ryl00@Remis-MacBook-Pro-M1.local>

---
+++ nvim/NVChad/custom/configs/null-ls.lua
@@ -0,0 +1,45 @@
+local null_ls = require "null-ls"
+
+local b = null_ls.builtins
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local sources = {
+
+  -- webdev stuff
+  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+
+  -- Lua
+  b.formatting.stylua,
+
+  -- cpp
+  b.formatting.clang_format,
+
+  b.diagnostics.buf,
+  b.diagnostics.tsc,
+  b.diagnostics.editorconfig_checker,
+
+  b.formatting.gofmt,
+  b.formatting.goimports,
+}
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+          --[[ vim.lsp.buf.formatting_sync() ]]
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
+}
---
+++ nvim/NVChad/custom/configs/overrides.lua
@@ -0,0 +1,68 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "json",
+    "c",
+    "markdown",
+    "markdown_inline",
+    "go",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+
+    -- golang
+    "gopls",
+    "goimports",
+
+    -- misc
+    "editorconfig-checker",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
