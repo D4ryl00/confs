63c7c6bf28e7ee5d43117516bc39ef35c6826ea2
D4ryl00 <d4ryl00@gmail.com>
2023-09-21 15:19:32 +0200

    add copilot
    Signed-off-by: D4ryl00 <d4ryl00@gmail.com>

--- nvim/NVChad/custom/plugins.lua
+++ nvim/NVChad/custom/plugins.lua
@@ -84,6 +84,79 @@ local plugins = {
     lazy = false,
   },
 
+  -- copilot
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+    config = function(_, opts)
+      require("copilot").setup(opts)
+    end,
+  },
+
+  -- copilot cmp source
+  {
+    "hrsh7th/nvim-cmp",
+    -- load cmp on InsertEnter
+    event = "InsertEnter",
+    -- these dependencies will only be loaded when cmp loads
+    -- dependencies are always lazy-loaded unless specified otherwise
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      {
+        "zbirenbaum/copilot-cmp",
+        opts = {},
+        dependencies = "copilot.lua",
+        config = function(_, opts)
+          local copilot_cmp = require "copilot_cmp"
+          copilot_cmp.setup(opts)
+          -- attach cmp source whenever copilot attaches
+          -- fixes lazy-loading issues with the copilot cmp source
+        end,
+      },
+    },
+    opts = {
+      sources = {
+        -- Copilot Source
+        { name = "copilot", group_index = 2 },
+        -- Other Sources
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "path", group_index = 2 },
+        { name = "luasnip", group_index = 2 },
+      },
+      mapping = {
+        ["<Tab>"] = vim.schedule_wrap(function(fallback)
+          local cmp = require "cmp"
+          local has_words_before = function()
+            if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+              return false
+            end
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match "^%s*$" == nil
+          end
+          if cmp.visible() and has_words_before() then
+            cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
+          else
+            fallback()
+          end
+        end),
+      },
+    },
+    config = function(_, opts)
+      require("cmp").setup(opts)
+    end,
+  },
+
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
